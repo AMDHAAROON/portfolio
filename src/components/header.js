2dff07b963c359a943510dc474ad72929b92d1b6
AMDHAAROON <mdhaaroon2020@gmail.com>
2024-12-11 16:08:48 +0530

    transforming header section in to responsive

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,33 @@
+import { Bars3Icon} from '@heroicons/react/24/solid'
+import { useState } from 'react'
+
+
 export default function Header(){
+   const [toggleMenu,settoggleMenu]= useState(false);
     return <>
-    <section className="p-6 ">    
-    <header className="flex justify-between px-10 pt-8 bg-primary rounded-t-2xl ">
-        <a href="#" className="font-bold text-2xl pb-2 ">Haaroon</a>
-        <ul className="flex text-white pb-2 font-marker ">
+    <section className="p-6 bg-white ">    
+    <header className="flex justify-between px-10 pt-8 bg-lavender rounded-t-2xl ">
+        <a href="#" className="font-cursive text-black text-3xl pb-2 ">Haaroon</a>
+        <nav className="hidden  md:block">
+        <ul className="flex  pb-2  text-white font-marker text-2xl ">
            <li> <a href="#">Home</a></li>
            <li> <a href="#">About</a></li>
             <li><a href="#">Projects</a></li>
             <li><a href="#">Contact</a></li>
         </ul>
+        </nav> 
+      
+       
+        { toggleMenu && <nav className="block md:hidden  ">
+        <ul className="flex flex-col    text-gray-500 font-marker text-3xl  pt-3 mobile-nav bg-opacity-45 ">
+           <li> <a href="#">Home</a></li>
+           <li> <a href="#" >About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        </nav>}
+        
+        <button onClick={()=>settoggleMenu(!toggleMenu)} className='block md:hidden'> <Bars3Icon className=' text-white h-10 pb-2 '/></button>
     </header>
     </section>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     extend: {
       fontFamily:{
         marker:['Concert One'],
+        chalk:['Edu AU VIC WA NT Arrows'],
+        cursive:['Cedarville Cursive'],
 
+      },
+      colors:{
+        flame:"#E1551E",
+        richblack:"#11151C",
+        lavender:"#B0BEE6",
+        lightlaven:"#CCD6F1",
       },
     },
   },
