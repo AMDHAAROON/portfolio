1176bd9ce379d3a0ab8e58b5ecd22da63e79948a
AMDHAAROON <mdhaaroon2020@gmail.com>
2024-12-17 17:44:58 +0530

    color changes

--- src/components/hero.js
+++ src/components/hero.js
@@ -16,7 +16,7 @@ return<div className='pb-4 dark'>
  <section className='flex  flex-col md:flex-row bg-lightlaven  mx-6  py-20  px-6 rounded-b-2xl  text-black  '>
     <div className='md:w-1/2  text-4xl font-chalk    md:pt-8 text-center md:ml-20 leading-normal md:leading-snug text-black flex flex-col' >
     <h1 >Hey !<br/>I'm Mohamed<span className='text-black font-marker hover:text-white cursor-pointer'> Haaroon,</span>
-    <p className='text-2xl font-mono mt-2'>{config.subtitle}</p>
+    <p className='text-lg font-mono mt-2'>{config.subtitle}</p>
      </h1>
      <div className='flex  mx-auto  md:justify-center mt-6'>
         <a href={config.social.linked} className='ml-12 mr-10 text-white hover:text-black'><FaLinkedin size={40}/></a>
