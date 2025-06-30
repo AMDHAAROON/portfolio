e90b3069baadb2bc63b79220cebf2479061c210a
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-06-30 22:20:31 +0530

    changed the color palette and added some animations

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,18 @@
 import aboutimg from '../assets/hero.png'
-export default function(){
+import { useInView } from 'react-intersection-observer';
+import Typewriter from 'typewriter-effect';
+import { useEffect, useState } from 'react';
+
+export default function TypingHeading() {
+ const { ref, inView } = useInView({ threshold: 0.5 });
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    if (inView) {
+      setKey(prev => prev + 1); // Force remount on view
+    }
+  }, [inView]);
+
 
     const config={
         line1:"I am Mohamed Haaroon, a BCA student at SASTRA University with a strong foundation in IT, data analytics, and programming.",
@@ -7,17 +20,27 @@ export default function(){
         line3:"Currently, I am enhancing my skills in front-end development and am eager to work as an intern or in any opportunity that allows me to contribute, learn, and grow in the IT field.",
     }
     
-    return<div className='px-6 pb-4 dark cursor-default'id='about' >
-    <section className='flex flex-col md:flex-row bg-flame rounded-2xl p-12 md:p-20 '  >
+    return<div className='px-6 pb-4 dark cursor-default 'id='about' >
+    <section className='flex flex-col md:flex-row border-2 border-lightlaven  shadow-2xl rounded-2xl p-12 md:p-20 '  >
         <div className='md:w-1/2  md:mr-10 '>
-            <img src={aboutimg} className='md:h-auto  md:mx-6 md:my-10 lg:my-0 '/>
+            <img src={aboutimg} className='md:h-auto  md:mx-6 md:my-10 lg:my-0 float-img'/>
         </div>
+        
         <div className='md:w-1/2 '>
+        
         <div className='flex flex-col  md:pl-8 '>
-            <h1 className='text-center text-2xl md:text-4xl text-white font-marker pb-2 border-dkflame border-b-4 dark:border-white'>Get to Know Me</h1>
-            <p className='pt-12 text-1xl font-design text-white'> {config.line1}</p>
-            <p className='pt-6 text-1xl font-design text-white'>{config.line2} </p>
-            <p className='pt-6 text-1xl font-design text-white '>{config.line3}</p>
+            <h1 ref={ref} className='text-[#7596F7] text-2xl md:text-5xl  font-cal  pb-2 tracking-wide'>
+                  {inView && (
+        <Typewriter
+          key={key} // this remounts the component
+          onInit={(typewriter) => {
+            typewriter.typeString('Get to know me').start();
+          }}
+        />
+      )} </h1>
+            <p className='pt-12 text-1xl font-mono '> {config.line1}</p>
+            <p className='pt-6 text-1xl font-mono'>{config.line2} </p>
+            <p className='pt-6 text-1xl font-mono '>{config.line3}</p>
             </div>
         </div>
     </section>
