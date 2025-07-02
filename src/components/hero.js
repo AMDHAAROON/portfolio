8db5e950658fd50e9dbf810760521cd9660d6786
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-07-02 22:55:30 +0530

    updated bg to light gray and added border lines to keep elevated

--- src/components/hero.js
+++ src/components/hero.js
@@ -12,8 +12,8 @@ export default function Hero(){
       }
    }
 
-return<div className='pb-4 cursor-default' id="hero">
- <section className='flex  flex-col md:flex-row bg-lightlaven  mx-6  py-16  px-6   text-black  '>
+return<div className='pb-4 cursor-default bg-[#e0e0e0]  ' id="hero">
+ <section className='flex  flex-col md:flex-row bg-lightlaven  mx-3  pt-16 pb-8  px-6   text-black border-x-2 border-[#ffff] '>
     <div className='md:w-1/2  text-4xl font-chalk  justify-center  md:pt-8 text-center md:ml-20 leading-normal md:leading-snug text-black flex flex-col' >
     <h1 >Hey !<br/>I'm Mohamed<span className='text-black font-marker hover:text-white cursor-pointer'> Haaroon,</span>
     <p className='text-lg md:text-xl font-mono mt-2'>{config.subtitle}<span className='caret'></span></p>
@@ -26,7 +26,7 @@ return<div className='pb-4 cursor-default' id="hero">
      <img  className=" md:w-1/2 mr-5 pt-8 float-img" src={heroimg}/>
    
     </section>
-    <div class="flex justify-center bg-lightlaven rounded-b-2xl py-4 mx-6">
+    <div class="flex justify-center bg-lightlaven rounded-b-2xl  py-4 mx-3 shadow-xl border-b-2 border-x-2  border-[#ffffff]">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5" />
               </svg>
